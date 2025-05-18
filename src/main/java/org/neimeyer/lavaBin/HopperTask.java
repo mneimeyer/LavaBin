9100b436518b55f3463682d1af5bc035e77f6c51
Matt Neimeyer <matt@neimeyer.org>
2025-05-18 13:25:10 -0400

    Remove excess debug logging

--- src/main/java/org/neimeyer/lavaBin/HopperTask.java
+++ src/main/java/org/neimeyer/lavaBin/HopperTask.java
@@ -12,15 +12,15 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class HopperTask extends BukkitRunnable {
   private final JavaPlugin plugin;
-
-  // map each hopper‐location to how many ticks left before next delete
   private final Map<Location, Integer> cooldowns = new HashMap<>();
 
-  /** 8 ticks is the vanilla hopper transfer cooldown */
   private static final int HOPPER_COOLDOWN = 8;
 
   public HopperTask(JavaPlugin plugin) {
@@ -29,9 +29,6 @@ public class HopperTask extends BukkitRunnable {
 
   @Override
   public void run() {
-    plugin.getLogger().info("[LavaBin] HopperTask tick");
-
-    // track which hoppers we actually see this tick
     Set<Location> seen = new HashSet<>();
 
     for (World world : plugin.getServer().getWorlds()) {
@@ -40,42 +37,29 @@ public class HopperTask extends BukkitRunnable {
           if (!(state instanceof Hopper)) continue;
 
           Hopper hopper = (Hopper) state;
-          Block hb = hopper.getBlock();
-          Location loc = hb.getLocation();
+          Location loc = hopper.getBlock().getLocation();
           seen.add(loc);
 
-          plugin.getLogger().info("[LavaBin]  ▶ Found Hopper @ " +
-                  loc.getBlockX() + "," +
-                  loc.getBlockY() + "," +
-                  loc.getBlockZ());
-
-          // decrement existing cooldown (if any)
           int cd = cooldowns.getOrDefault(loc, 0);
           if (cd > 0) {
             cooldowns.put(loc, cd - 1);
             continue;
           }
 
-          // check if it’s pointing into a full lava cauldron
           org.bukkit.block.data.type.Hopper data =
-                  (org.bukkit.block.data.type.Hopper) hb.getBlockData();
+                  (org.bukkit.block.data.type.Hopper) hopper.getBlock().getBlockData();
           BlockFace face = data.getFacing();
-          Block target = hb.getRelative(face);
+          Block target = hopper.getBlock().getRelative(face);
           if (target.getType() != Material.LAVA_CAULDRON) {
-            // not the right target, leave cd at zero
             continue;
           }
 
-          plugin.getLogger().info("[LavaBin]     ✓ Deleting 1 item from hopper");
           deleteOneItem(hopper);
-
-          // reset this hopper’s cooldown
           cooldowns.put(loc, HOPPER_COOLDOWN);
         }
       }
     }
 
-    // cleanup hoppers that have gone out of scope (unloaded etc)
     cooldowns.keySet().removeIf(k -> !seen.contains(k));
   }
 
@@ -84,7 +68,6 @@ public class HopperTask extends BukkitRunnable {
     for (int i = 0; i < inv.getSize(); i++) {
       ItemStack stack = inv.getItem(i);
       if (stack != null && stack.getType() != Material.AIR) {
-        // remove a single item from that slot
         stack.setAmount(stack.getAmount() - 1);
         if (stack.getAmount() <= 0) {
           inv.setItem(i, null);
